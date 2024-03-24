c177ae3758cd5a7ca49e7386d3f7990faae94c18
AHoch1366 <158213894+AHoch1366@users.noreply.github.com>
2024-03-24 11:20:51 -0700

    week 8 homework complete

---
+++ week 8/assets/scripts/functions.js
@@ -0,0 +1,54 @@
+let gasLevel = 0;
+let tracLevel = 0;
+let battLevel = 0;
+let parkLevel = 0;
+
+showTime();
+
+function getGas() {
+    if (gasLevel == 0) {
+        gasLevel = 1;
+        document.getElementById('gas').src = './assets/images/gas1.png';
+        document.getElementById('bttn_gas').src = './assets/images/bttn_gas_1.png';
+    } else {
+        gasLevel = 0;
+        document.getElementById('gas').src = './assets/images/gas0.png';
+        document.getElementById('bttn_gas').src = './assets/images/bttn_gas_0.png';
+    }
+}
+
+function getTrac() {
+    if (tracLevel == 0){
+        tracLevel = 1;
+        document.getElementById('trac').src = './assets/images/trac0.PNG';
+        document.getElementById('bttn_trac').src = './assets/images/bttn_trac_1.PNG';
+    } else {
+        tracLevel = 0;
+        document.getElementById('trac').src = './assets/images/trac1.PNG';
+        document.getElementById('bttn_trac').src = './assets/images/bttn_trac_0.PNG';
+    }
+    }
+function getBatt() {
+    if (battLevel == 0) {
+        battLevel = 1;
+        document.getElementById('batt').src = './assets/images/batt1.png';
+        document.getElementById('bttn_batt').src = './assets/images/bttn_batt_1.png';
+    } else {
+        battLevel = 0;
+        document.getElementById('batt').src = './assets/images/batt0.png';
+        document.getElementById('bttn_batt').src = './assets/images/bttn_batt_0.png';
+    }
+    }
+function getPark(){
+    if (parkLevel == 0) {
+        parkLevel = 1;
+        document.getElementById('brake').src = './assets/images/p1.PNG';
+        document.getElementById('bttn_park').src = './assets/images/bttn_park_1.PNG';
+    }
+    else {
+        parkLevel = 0;
+        document.getElementById('brake').src = './assets/images/p0.PNG';
+        document.getElementById('bttn_park').src = './assets/images/bttn_park_0.PNG'
+
+    }
+    }
